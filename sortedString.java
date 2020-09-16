53a73e0ec1e59da03b71126b2c92f196395bd24e
tahailyas <43189166+tahailyas@users.noreply.github.com>
2020-09-16 02:46:44 -0700

    Add files via upload

---
+++ sortedString.java
@@ -0,0 +1,24 @@
+package testrun;
+
+import java.util.Arrays;
+
+public class sortedString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] exampleArray = {"Taha", "Mads", "Football", "Rugby", "Cricket", "Soccer"};
+	      int size = exampleArray.length;
+
+	      for(int i = 0; i<size-1; i++) {
+	         for (int j = i+1; j<exampleArray.length; j++) {
+	            if(exampleArray[i].compareTo(exampleArray[j])>0) {
+	               String temp = exampleArray[i];
+	               exampleArray[i] = exampleArray[j];
+	               exampleArray[j] = temp;
+	            }
+	         }
+	      }
+	      System.out.println(Arrays.toString(exampleArray));
+	}
+
+}
